8035e3d4bb589cd964008999b0b52300fd757ef5
Shunsuke Matsuno <matsuno.shunsuke@gmail.com>
2021-07-24 16:00:30 +0900

    Changed the computation of Gamma. - Fixed the random draw for all grid points of Gamma.

--- R/compute_Gamma.R
+++ R/compute_Gamma.R
@@ -1,5 +1,9 @@
-compute_Gamma <- function(y, p, sigma_x, alpha, beta){
-  # The function that defines P_nd as a fixed point.
+compute_Gamma <- function(p, sigma_x, alpha, beta,
+                          interval = c(-1.5, .5)){
+  # @description
+  #  Returns the function that defines P_nd as a fixed point. 
+  #  Uses spline interpolation and returns the splined function
+  
   
   # integrand
   g <- function(xi, y, p, sigma_x, alpha, beta){
@@ -7,10 +11,22 @@ compute_Gamma <- function(y, p, sigma_x, alpha, beta){
   }
   g <- Vectorize(g, "xi")
   
+  # Integral
+  compute_Gamma_val <- function(y, xi, 
+                        p, sigma_x, alpha, beta){
+    # returns Gamma for each y
+    I <- mean(g(xi, y, p, sigma_x, alpha, beta))
+    return((1-p)/p * I)
+  }
+  
   # simulate normal variables and take mean
   xi <- rnorm(1e4)
-  I <- mean(g(xi, y, p, sigma_x, alpha, beta))
+  df_Gamma <- tibble(y = seq(interval[1], interval[2], .1)) %>% 
+    mutate(Gamma_val = map_dbl(.x = y, .f = compute_Gamma_val, xi = xi,
+                               p = p, sigma_x = sigma_x, alpha = alpha, beta = beta)) 
+  Gamma_splined <- splinefun(x = df_Gamma %>% pull(y),
+                             y = df_Gamma %>% pull(Gamma_val))
   
-  # return the value of gamma
-  return((1-p)/p * I)
+  return(list(df = df_Gamma, fun = Gamma_splined))
 }
+
--- R/compute_fixedpt_Gamma.R
+++ R/compute_fixedpt_Gamma.R
@@ -5,31 +5,11 @@ compute_fixedpt_Gamma <- function(p, sigma_x, alpha, beta, max_iter = 500){
   # @note 
   # The initial value is important. We set y_init = -0.25 + 0.2 * p
   
+  # splined gamma
+  Gamma_splined <- compute_Gamma(p, sigma_x, alpha, beta)$fun
   
-  # spline approximation of Gamma
-  df_Gamma <- tibble(y = seq(-2, .5, .1)) %>% 
-    mutate(Gamma_val = map_dbl(.x = y, .f = compute_Gamma,
-                               p = p, sigma_x = sigma_x, alpha = alpha, beta = beta)) 
-  Gamma_splined <- splinefun(x = df_Gamma %>% pull(y),
-                             y = df_Gamma %>% pull(Gamma_val))
   f <- function(x) Gamma_splined(x) - x
   res <- rootSolve::multiroot(f, start = -0.25 + 0.2 * p)
   return(res$root)
   
-  # tolerance <- 1e-4
-  # error <- 100
-  # count <- 0
-  # y_old <- -.25 + .2 * p   # initial value
-  # while(error > tolerance & count < max_iter){
-  #   count <- count + 1
-  #   y_new <- Gamma_splined(y_old)
-  #   print(y_new)
-  #   error <- abs(y_new - y_old)
-  #   y_old <- y_new   # update
-  # }
-  # if(count == max_iter){
-  #   print("convergence failed!!!")
-  # }
-  # 
-  # return(y_old)
 }
